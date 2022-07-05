6675461ee66f90071195a8af087171f6b41a74af
Gonzalo Varela <gonzalo.varela1981@gmail.com>
2022-07-05 10:10:58 -0300

    Remove duplicate algortim

--- matrix/2d-matrix-algoritm.py
+++ matrix/2d-matrix-algoritm.py
@@ -1,13 +1,15 @@
-def make_matrix(list):
+def make_matrix(relations):
     """
     Create a 2d matrix (dictionary filled with lists) from key:value lists
     input: list of 2 itmes list
-           example: [[1, 2], [2, 3], [4, 3]]
-    output: 2d matrix where index are the first element of every input list
-           example: {1:[2], 2:[3], 4:[3]}
+           example 1: [['Carlos', 'Luis'], ['Maria', 'Luis], ['Carlos', 'Jenny']]
+           example 2: [[1, 2], [2, 3], [4, 3]]
+    output: 2d matrix (dictionary) where keys are the first element of every input list
+           example 1: {'Carlos':['Luis', 'Jenny'], 'Maria':['Luis']}
+           example 2: {1:[2], 2:[3], 4:[3]}
     """
     matrix = {}
-    for relation in list:
+    for relation in relations:
         row = relation[0]
         col = relation[1]
         try:
@@ -37,3 +39,14 @@ def get_matrix_single_values_and_keys(matrix):
             if value not in matrix_values: matrix_values.append(value)
     return matrix_values
 
+
+def remove_list_duplicates(listed_values):
+    """
+    Given a list of values with duplicates return a list with no duplicates
+    List may come from a from values of a matrix
+    """
+    response_list = []
+    for value in listed_values:
+        if value not in response_list:
+            response_list.append(value)
+    return response_list
